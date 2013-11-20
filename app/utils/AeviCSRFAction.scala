b29c332028af44207c20fd6fd37eb847b573adc9
Jianfeng Tian <jianfeng.tian@gmail.com>
2013-11-20 15:00:32 +0000

    add new logic

--- app/utils/AeviCSRFAction.scala
+++ app/utils/AeviCSRFAction.scala
@@ -22,28 +22,18 @@ import play.api.mvc.Results._
 import play.api.libs.json.JsValue
 import play.api.libs.json.JsSuccess
 import play.api.libs.json.JsError
-
+import scala.concurrent.ExecutionContext.Implicits.global
+import javax.crypto.IllegalBlockSizeException
 package object AeviSecurity {
+
+    val getCipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+
     val getKey: Key = {
         val applicationSecret = current.configuration.getString("application.secret").getOrElse(throw new Exception("application secret is not configured"))
 
         val sha = MessageDigest.getInstance("SHA-1")
         val trimedKey = Arrays.copyOf(sha.digest(applicationSecret.getBytes("UTF-8")), 16)
         new SecretKeySpec(trimedKey, "AES")
-    }
-    val getCipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
-}
-
-class CsrfRequest[A](val username: String, request: Request[A]) extends WrappedRequest[A](request)
-
-object GetAction extends ActionBuilder[Request] {
-    def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[SimpleResult]) = {
-        if (request.headers.get("token") == None) {
-            val username = request.headers.get("username").get
-            val token = encrypt(username)
-            println("token => " + token)
-        }
-        block(request)
     }
 
     def encrypt(username: String): String = {
@@ -57,41 +47,60 @@ object GetAction extends ActionBuilder[Request] {
         val cipherText = cipher.doFinal(payload)
         new String(Base64.encodeBase64(cipherText))
     }
-}
-
-object PostAction extends ActionBuilder[CsrfRequest] {
-    def invokeBlock[A](request: Request[A], block: (CsrfRequest[A]) => Future[SimpleResult]) = {
-        val decrypted = decrypt(request.headers.get("token").get)
-        println("decrypted => " + decrypted)
-
-        val username = (Json.parse(decrypted) \ "username").as[String]
-        val expiry = (Json.parse(decrypted) \ "expiry").as[Long]
-        println("username => " + username)
-        println("expiry => " + expiry)
-        val jsPayload = request.body.asInstanceOf[AnyContentAsJson].asJson.get
-        val jsUsername = (jsPayload \ "username").asOpt[String].get
-
-        if (new DateTime(expiry).isBefore(DateTime.now))
-            Future.successful(BadRequest("The token is expired"))
-        else if (jsUsername != username)
-            Future.successful(BadRequest("The usernme is invalid"))
-        else {
-            block(new CsrfRequest(username, request))
-        }
-    }
 
     def decrypt(token: String) = {
         val key = AeviSecurity.getKey
-
         val cipher = AeviSecurity.getCipher
-        cipher.init(Cipher.DECRYPT_MODE, key);
 
+        cipher.init(Cipher.DECRYPT_MODE, key);
         val decoded = Base64.decodeBase64(token.getBytes)
         val newPlainText = cipher.doFinal(decoded);
         System.out.println("Finish decryption: ");
 
         new String(newPlainText, "UTF8")
-
     }
+}
+
+//class CsrfRequest[A](val username: String, request: Request[A]) extends WrappedRequest[A](request)
+
+object GetAction extends ActionBuilder[Request] {
+    def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[SimpleResult]) = {
+        request.headers.get("username") match {
+            case None => block(request)
+            case Some(username) => {
+                block(request) map (result => result.withHeaders("token" -> AeviSecurity.encrypt(username)))
+            }
+        }
+    }
+
+}
+
+object PostAction extends ActionBuilder[Request] {
+    def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[SimpleResult]) = {
+        (request.headers.get("username"), request.headers.get("token")) match {
+            case (Some(username), Some(token)) => {
+                try {
+                    val decryptedToken = Json.parse(AeviSecurity.decrypt(token))
+
+                    ((decryptedToken \ "username").asOpt[String], (decryptedToken \ "expiry").asOpt[Long]) match {
+                        case (Some(tokenUsername), Some(tokenExpiry)) => {
+                            if (new DateTime(tokenExpiry).isBefore(DateTime.now))
+                                Future.successful(BadRequest("The token is expired"))
+                            else if (tokenUsername != username)
+                                Future.successful(BadRequest("The usernme is invalid"))
+                            else {
+                                block(request) map (result => result.withHeaders("token" -> AeviSecurity.encrypt(username)))
+                            }
+                        }
+                        case _ => Future.successful(BadRequest("The token is invalid"))
+                    }
+                } catch {
+                    case e: IllegalBlockSizeException => Future.successful(BadRequest("The token is damaged1"))
+                }
+            }
+            case _ => Future.successful(BadRequest("The request header is invalid"))
+        }
+    }
+
 }
 
